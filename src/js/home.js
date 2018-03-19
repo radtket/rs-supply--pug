d7c57f7ce67b3fda216e932117866c961d28f47c
Taylor Radtke <taylorradtke@gmail.com>
2018-03-19 08:56:30 -0400

    added tabs nav underline

--- src/js/home.js
+++ src/js/home.js
@@ -55,9 +55,69 @@ $('.accordion').each(function() {
 		});
 });
 
+function initTabsNav() {
+	const $line = $('.tabs-nav-underline');
+	const $navListItem = $('.nav-li');
+	const $activeWidth = $('.active-nav').width();
+	const $firstChild = $('.nav-li:first-child');
+	const $defaultMarginLeft = parseInt(
+		$('.nav-li:first-child')
+			.next()
+			.css('marginLeft')
+			.replace(/\D/g, '')
+	);
+	const $defaultPaddingLeft = parseInt(
+		$('.tabs-nav-wrapper > ul')
+			.css('padding-left')
+			.replace(/\D/g, '')
+	);
+
+	$line.width(`${$activeWidth}px`);
+
+	$line.css('marginLeft', `${$defaultPaddingLeft}px`);
+
+	$navListItem.click(function() {
+		let $activeNav, $currentIndex, $currentOffset, $currentWidth, $initWidth, $marginLeftToSet, $this;
+		$this = $(this);
+		$activeNav = $('.active-nav');
+		$currentWidth = $activeNav.width();
+		$currentOffset = $activeNav.position().left;
+		$currentIndex = $activeNav.index();
+		$activeNav.removeClass('active-nav');
+		$this.addClass('active-nav');
+		if ($this.is($activeNav)) {
+			return 0;
+		}
+		if ($this.index() > $currentIndex) {
+			if ($activeNav.is($firstChild)) {
+				$initWidth = $defaultMarginLeft + $this.width() + $this.position().left - $defaultPaddingLeft;
+			} else {
+				$initWidth = $this.position().left + $this.width() - $currentOffset;
+			}
+			$marginLeftToSet = `${$this.position().left + $defaultMarginLeft}px`;
+			$line.width(`${$initWidth}px`);
+			return setTimeout(() => {
+				$line.css('marginLeft', $marginLeftToSet);
+				return $line.width(`${$this.width()}px`);
+			}, 175);
+		}
+		if ($this.is($firstChild)) {
+			$initWidth = $currentOffset - $defaultPaddingLeft + $defaultMarginLeft + $currentWidth;
+			$marginLeftToSet = $this.position().left;
+		} else {
+			$initWidth = $currentWidth + $currentOffset - $this.position().left;
+			$marginLeftToSet = $this.position().left + $defaultMarginLeft;
+		}
+		$line.css('marginLeft', $marginLeftToSet);
+		$line.width(`${$initWidth}px`);
+		return setTimeout(() => $line.width(`${$this.width()}px`), 175);
+	});
+}
+
 $(document).ready(() => {
 	$(window).trigger('resize');
 	initTabs();
+	initTabsNav();
 });
 
 $(window).resize(() => {
--- src/js/store-picker.js
+++ src/js/store-picker.js
@@ -441,7 +441,7 @@ function createPopUp(currentFeature) {
 		popUps[0].remove();
 	}
 
-	const popup = new mapboxgl.Popup({
+	new mapboxgl.Popup({
 		closeOnClick: false,
 	})
 		.setLngLat(currentFeature.geometry.coordinates)
@@ -484,14 +484,15 @@ function buildLocationList(data) {
 				prop.address
 			}. ${prop.city}, ${prop.state} ${prop.postalCode}</li>
 		</ul>
-		<ul>
-			<li style="display: inline-block"><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348.1 347.3"><path d="M340.3 274.7l-53.8-53.8c-10.7-10.6-28.4-10.3-39.5.8l-27.1 27-5.3-2.9c-17.1-9.5-40.5-22.5-65.2-47.1s-37.7-48.2-47.2-65.3c-1-1.8-1.9-3.6-2.9-5.2l18.2-18.2 8.9-8.9c11.1-11.1 11.4-28.8.7-39.5L73.4 7.8C62.7-2.9 45-2.6 33.9 8.5L18.7 23.8l.4.4a87.5 87.5 0 0 0-12.4 22 86.8 86.8 0 0 0-5.6 22.3C-6 127.4 20.9 181.2 93.9 254.1 194.7 355 276 347.4 279.5 347a90.4 90.4 0 0 0 44.4-18l.3.3 15.4-15.1c11-11.1 11.4-28.8.7-39.5z"/></svg></span>${
+		<ul style="display: inline-block">
+			<li><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348.1 347.3"><path d="M340.3 274.7l-53.8-53.8c-10.7-10.6-28.4-10.3-39.5.8l-27.1 27-5.3-2.9c-17.1-9.5-40.5-22.5-65.2-47.1s-37.7-48.2-47.2-65.3c-1-1.8-1.9-3.6-2.9-5.2l18.2-18.2 8.9-8.9c11.1-11.1 11.4-28.8.7-39.5L73.4 7.8C62.7-2.9 45-2.6 33.9 8.5L18.7 23.8l.4.4a87.5 87.5 0 0 0-12.4 22 86.8 86.8 0 0 0-5.6 22.3C-6 127.4 20.9 181.2 93.9 254.1 194.7 355 276 347.4 279.5 347a90.4 90.4 0 0 0 44.4-18l.3.3 15.4-15.1c11-11.1 11.4-28.8.7-39.5z"/></svg></span>${
 				prop.phone
 			}</li>
-			<li style="display: inline-block"><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.73 31.73"><path d="M0 29.66h2.537V9.571H0V29.66zM28.549 9.571V.917H15.871v8.654H10.91v20.09h20.82V9.571h-3.181zm-8.926 15.908h-2.447v-2.446h2.447v2.446zm0-4.045h-2.447V18.99h2.447v2.444zm0-3.855h-2.447v-2.446h2.447v2.446zm4.516 7.9h-2.447v-2.446h2.447v2.446zm0-4.045h-2.447V18.99h2.447v2.444zm0-3.855h-2.447v-2.446h2.447v2.446zm4.513 7.9h-2.445v-2.446h2.445v2.446zm0-4.045h-2.445V18.99h2.445v2.444zm0-3.855h-2.445v-2.446h2.445v2.446zm1.506-4.574H14.26V10.84h1.611v1.082h12.678V10.84h1.609v2.165zM3.535 30.813h6.398V7.332H3.535v23.481zm23.33-27.486h-9.029V2.48h9.029v.847zm0 2.256h-9.029v-.844h9.029v.844z"/></svg></span>${
+			<li><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.73 31.73"><path d="M0 29.66h2.537V9.571H0V29.66zM28.549 9.571V.917H15.871v8.654H10.91v20.09h20.82V9.571h-3.181zm-8.926 15.908h-2.447v-2.446h2.447v2.446zm0-4.045h-2.447V18.99h2.447v2.444zm0-3.855h-2.447v-2.446h2.447v2.446zm4.516 7.9h-2.447v-2.446h2.447v2.446zm0-4.045h-2.447V18.99h2.447v2.444zm0-3.855h-2.447v-2.446h2.447v2.446zm4.513 7.9h-2.445v-2.446h2.445v2.446zm0-4.045h-2.445V18.99h2.445v2.444zm0-3.855h-2.445v-2.446h2.445v2.446zm1.506-4.574H14.26V10.84h1.611v1.082h12.678V10.84h1.609v2.165zM3.535 30.813h6.398V7.332H3.535v23.481zm23.33-27.486h-9.029V2.48h9.029v.847zm0 2.256h-9.029v-.844h9.029v.844z"/></svg></span>${
 				prop.fax
 			}</li>
 		</ul>
+		<dl class="store__manager"><dt>Store Manager</dt><dd>Alan Shufelberger</dd></dl>
 
 		`;
 
@@ -500,7 +501,7 @@ function buildLocationList(data) {
 			details.innerHTML += `<p><strong>${roundedDistance} miles away</strong></p>`;
 		}
 
-		link.addEventListener('click', () => {
+		link.addEventListener('click', function() {
 			// Update the currentFeature to the store associated with the clicked link
 			const clickedListing = data.features[this.dataPosition];
 
